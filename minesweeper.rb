b8a7a269fe423c2fe347643e7e51d1f5146cca8a
App Academy <app-academy@rodin.att.net>
2013-01-15 09:58:37 -0800

    Built out a board class for 9/16 sized boards

---
+++ minesweeper.rb
@@ -0,0 +1,44 @@
+class Board
+  attr_reader :game_board
+
+  def initialize(size)
+    @game_board = place_bombs(blank_board(size))
+  end
+
+  def blank_board(size)
+    empty_board = []
+    size.times do
+      empty_row = []
+      size.times { empty_row << " " }
+      empty_board << empty_row
+    end
+    p empty_board
+  end
+
+  def place_bombs(board)
+    num_of_bombs(board).times do
+      row, column = choose_bomb_location(board)
+      while board[row][column] == "B"
+        row, column = choose_bomb_location(board)
+      end
+      board[row][column] = "B"
+    end
+    board
+  end
+
+  def num_of_bombs(board)
+    if board.size == 9
+      return 10
+    elsif board.size == 16
+      return 40
+    end
+  end
+
+
+  def choose_bomb_location(board)
+    row = rand(0..board.length - 1)
+    column = rand(0..board.length - 1)
+    [row, column]
+  end
+end
+
