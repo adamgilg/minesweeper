adaf6b71d473af30ae5aabf7bcd14b1f5eb47541
Adam G <adam.gilg@gmail.com>
2013-01-15 15:15:33 -0800

    Added a working adjacents method using DELTAS constant

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,9 +1,21 @@
+#require 'debugger'
 class Board
+  DELTAS = [[1, 1],
+  [1, 0],
+  [1, -1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+  [0, 1],
+  [0, -1]
+  ]
+
   attr_reader :game_board
 
   def initialize(size)
     @game_board = place_bombs(blank_board(size))
   end
+
 
   def blank_board(size)
     empty_board = []
@@ -23,6 +35,7 @@ class Board
       end
       board[row][column].bomb = true
     end
+    board
   end
 
   def num_of_bombs(board)
@@ -39,26 +52,57 @@ class Board
     [row, column]
   end
 
-  # def display_bomb_board
-  #   @game_board.each do |row|
-  #     row.each do |item|
-  #       print "#{item.bomb} "
-  #     end
-  #     puts ""
-  #   end
-  # end
+  def display_bomb_board
+    @game_board.each do |row|
+      row.each do |item|
+        print "#{item.adj_bombs}|#{item.bomb} "
+      end
+      puts ""
+    end
+  end
 
+  def adjacents(row, column)
+    adjacents = []
 
+    DELTAS.each do |delta|
+      new_row = row + delta[0]
+      new_column = column + delta[1]
+      if (0..@game_board.size - 1).include?(new_row) && (0..@game_board.size - 1).include?(new_column)
+        adjacents << @game_board[new_row][new_column]
+      end
+    end
+    adjacents
+  end
+
+  def visit_adjacents(&set_adj)
+    @game_board.each_with_index do |row, row_index| 
+      row.each_with_index do |item, column_index|
+
+        adjacents_array = adjacents(row_index, column_index)
+        adjacents_array.each do |adjacent_item|
+          #this line will be passed in as a block
+          #ask Ned about passing in multiple variables to a block
+          #item.adj_bombs += 1 if adjacent_item.bomb == true
+          set_adj.call(item, adjacent_item)
+        end
+      end
+    end
+  end
+          #blocks to be passed in to visit_adjacents
+          #{ |item, adjacent_item| item.adj_bombs += 1 if adjacent_item.bomb == true }
+          #this one is not finished.
+          # { continue going if item.adj_bombs == 0 }
 
 end
 
 class BoardPosition
-  attr_accessor :bomb, :visited, :adj_bombs
+  attr_accessor :bomb, :visited, :adj_bombs, :flag
 
-  def initialize(bomb=false, visited=false, adj_bombs=0)
+  def initialize(bomb=false, visited=false, adj_bombs=0, flag=false)
     @bomb = bomb
     @visited = visited
     @adj_bombs = adj_bombs
+    @flag = flag
   end
 
 end
